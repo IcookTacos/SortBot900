b1412724558b137f3be40c9d1105dd6e7f1f1d82
Erik Zeidlitz <zeidlitzerik@gmail.com>
2023-12-21 21:26:51 +0100

    Update Main.java
    Cleaning up dead code and comments

--- SortBot900_v2/src/Main.java
+++ SortBot900_v2/src/Main.java
@@ -2,24 +2,9 @@ import javax.swing.JFrame;
 
 @SuppressWarnings("unused")
 public class Main {
-
 	public static void main(String[] args) {
-		
-		// RUN COMPLETE BOT
 		Window w1 = new Window();
 		w1.setVisible(true);
 		w1.inputTxt.requestFocus();
-		
-		
-		//TESTING VIEWER
-		//Viewer v1 = new Viewer();
-		//v1.setVisible(true);
-		
-
-
 	}
-
-	
-
-
 }
